f3ab46a28d956fdae191a6ec7e70f8de1b80b814
Biswajeet Sahu <91553235+XiomGreninja@users.noreply.github.com>
2024-11-23 20:20:15 +0530

    Delete New folder/Login System directory

--- New folder/Login System/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        IDandPassword idandpasswords = new IDandPassword();
-
-        LoginPage loginpage = new LoginPage(idandpasswords.getLoginInfo());
-    }
-}
